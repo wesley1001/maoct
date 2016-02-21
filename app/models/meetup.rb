cb1bc7cc857cc3c080952d9234a2c4bb7a89cb57
goofansu <goofan.su@gmail.com>
2016-02-21 11:27:30 +0800

    set default time for datetime

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,3 +1,11 @@
 class Meetup < ActiveRecord::Base
   belongs_to :author, class_name: "User"
+
+  after_initialize :init
+
+  def init
+    self.open_at = Time.now
+    self.close_at = Time.now
+    self.deadline = Time.now
+  end
 end
