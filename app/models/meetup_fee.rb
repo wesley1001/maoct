d43859ad2f8f09027080e620eb0daf97bfc89e2b
goofansu <goofan.su@gmail.com>
2016-02-22 04:17:37 +0800

    set default quota_used

--- app/models/meetup_fee.rb
+++ app/models/meetup_fee.rb
@@ -1,6 +1,12 @@
 class MeetupFee < ActiveRecord::Base
   belongs_to :meetup
 
+  after_initialize :init
+
+  def init
+    self.quota_used ||= 0
+  end
+
   def value_show
     self.value / 100.0
   end
