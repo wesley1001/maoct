2fe8dc0fba437d6878cf5d6f2b4ad0a526d038dc
goofansu <goofan.su@gmail.com>
2016-02-22 08:45:52 +0800

    set fee value before save

--- app/models/meetup_fee.rb
+++ app/models/meetup_fee.rb
@@ -2,16 +2,19 @@ class MeetupFee < ActiveRecord::Base
   belongs_to :meetup
 
   after_initialize :init
+  before_save :normalize_value
 
   def init
     self.quota_used ||= 0
   end
 
-  def value_show
-    self.value / 100.0
-  end
-
   def quota_remained
     self.quota - self.quota_used
+  end
+
+  private
+  def normalize_value
+    self.value = self.real_value * 100
+    puts self.value
   end
 end
